c85c92993db0f02621f7822a616ffdd5a01e17e4
Aniket Singh <aniketsingh2151@gmail.com>
2025-07-26 04:49:13 +0530

    added user table, made id unique, finished signup and signin endpoints, jwt decoding on website endpoints left

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,24 +1,72 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 const app = express();
 // import v1Router from "./routes/v1";
 import { prismaClient } from "store/client";
+import { AuthInput } from "./types";
 
 app.use(express.json());
 // app.use("/v1", v1Router);
 
-app.post("/website", async (req, res) => {
-    if(!req.body.url) {
-        return res.status(411).json({});
-        return
+app.post("/user/signup", async (req, res) => {
+    const user_data = AuthInput.safeParse(req.body);
+    if (!user_data.success){
+        res.status(403).send("");
+        return;
     }
-    const website = await prismaClient.website.create({
-        data: {
-            url: req.body.url
-        }
-    });
+    try {
+        let user = await prismaClient.user.create({
+            data: {
+                username: user_data.data.username,
+                password: user_data.data.password
+            }
+        })
+        res.json({
+            id: user.id
+        })
+    } catch (error) {
+        res.status(403).send("");
+        return;
+    }
+})
 
-    res.json({ id:website.id });
-});
+app.post("/user/signin", async (req, res) => {
+    const user_data = AuthInput.safeParse(req.body);
+    if (!user_data.success){
+        res.status(403).send("");
+        return;
+    }
+    try {
+        let user = await prismaClient.user.findFirst({
+            where: {
+                username: user_data.data.username,
+                password: user_data.data.password
+            }
+        })
+        let token = jwt.sign({
+            sub: user?.id
+        }, process.env.JWT_SECRET!)
+        res.json({
+            jwt: token
+        })
+    } catch (error) {
+        res.status(403).send("");
+        return;
+    }
+})
+
+// app.post("/website", async (req, res) => {
+//     if(!req.body.url) {
+//         return res.status(411).json({});
+//     }
+//     const website = await prismaClient.website.create({
+//         data: {
+//             url: req.body.url
+//         }
+//     });
+
+//     res.json({ id:website.id });
+// });
 
 app.get("/status/:websiteId", (req, res) => {
     prismaClient.websiteTick.findMany({
