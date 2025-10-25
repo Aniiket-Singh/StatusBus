faabb99915bab772ece66673c0e4eb48febe273b
Aniket <aniketsingh2151@gmail.com>
2025-10-25 23:05:11 +0530

    the docekr containers are running but for some reason the docker ocntainer for api isnt behaving properly

--- apps/fe/next.config.ts
+++ apps/fe/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // output: export converts the output to SPA
+  // output: 'export',  
+  // distDir: 'dist',   
 };
 
 export default nextConfig;
