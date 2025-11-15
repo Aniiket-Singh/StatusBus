6b0a218abd0406567acc5aef9e5a37f878db905a
Aniket <aniketsingh2151@gmail.com>
2025-11-16 01:54:28 +0530

    dockerfie updates for docker-compose build

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,14 +1,19 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
+
 export const authMiddleWare = (req:Request, res:Response, next: NextFunction) => {
     const header = req.headers.authorization!;
+
     try { 
         let data = jwt.verify(header, process.env.JWT_SECRET!);
         req.user_id = data.sub as string;
         next();
-    }catch(e){
+    } catch (e){
         res.status(403).send("");
     }
-
 }
+
+
+
+
